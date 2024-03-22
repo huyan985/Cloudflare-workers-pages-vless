cc29e5e4cb18dc40bda0cebf5650032dbdd21e16
huyan985 <70787170+huyan985@users.noreply.github.com>
2024-03-22 09:12:12 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '77a571fb-4fd2-4b37-8596-1b7d9728bb5c';
+let userID = 'a85c762b-ce22-4224-84f9-0f7e55ef61b7';
 
 const proxyIPs = ['workers.bestip.one'];//['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org'];
 
